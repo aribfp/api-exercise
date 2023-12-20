a84fdc6d9b2406a2f22636925544a901bc9847e1
aribfp <aribpelayo@gmail.com>
2023-12-21 01:43:52 +1300

    added an output (dummy)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-  
+import { useState } from 'react';
+
 function App() {
+
+  const [word, setWord] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setWord(e.target.word_input.value)
+  }
+
   return (
     <div className="App">
-        <form>
-          <label for='word_input'> Input a word
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor='word_input'> Input a word
             <input type='text' id='word_input' />
           </label>
           <input type='submit' value='Submit' />
         </form>
+
+        <p> { word } </p>
     </div>
   );
 }
